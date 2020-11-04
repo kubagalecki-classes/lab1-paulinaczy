49aff8266ac4a646ea4c973239c7f486c8e486c6
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2020-11-04 14:05:42 +0000

    done

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,26 @@
+#include <iostream>
+using namespace std;
+
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    Wektor2D() { x = 0.; y = 0.; }//Konstruktor domyślny
+    void setX(double nowy_X) { x = nowy_X; }
+    void setY(double nowy_Y) { y = nowy_Y; }
+    double getX() { return x; }
+    double getY() { return y; }
+    Wektor2D(double nowy_X, double nowy_Y) { x = nowy_X; y = nowy_Y; }
+
+    friend Wektor2D operator+(Wektor2D& wek1, Wektor2D& wek2)
+    {
+        return Wektor2D(wek1.x + wek2.x, wek1.y + wek2.y);
+    }
+
+    friend double operator*(Wektor2D& wek1, Wektor2D& wek2)
+    {
+        return wek1.x * wek2.x + wek1.y * wek2.y;
+    }
+    //private:
+    double x;
+    double y;
 };
